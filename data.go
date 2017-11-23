53789222d636345b69d7abb49be349163b9deef8
Andres Erbsen <andreser@mit.edu>
2017-11-23 10:42:10 -0500

    signups until end of thanksgiving

--- data.go
+++ data.go
@@ -34,7 +34,7 @@ func GetDateRange() (startDate time.Time, endDate time.Time) {
 		panic(err)
 	}
 	startDate = time.Date(2017, 1, 2, 0, 0, 0, 0, EST)
-	endDate = time.Date(2017, 8, 31, 0, 0, 0, 0, EST)
+	endDate = time.Date(2017, 11, 26, 0, 0, 0, 0, EST)
 	return
 }
 
